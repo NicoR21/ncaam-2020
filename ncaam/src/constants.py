aef65320ce97ad60ba4ee2dcfef9fb4206e30cd1
p.steffen <p.steffen@betclicgroup.com>
2020-04-10 15:27:00 +0200

    update constants

--- ncaam/src/constants.py
+++ ncaam/src/constants.py
@@ -1,2 +1,7 @@
 DATA_PATH = "ncaam/resources/data"
+
+DATA_FILES = "MDataFiles"
+PLAY_BY_PLAY = "MPlayByPlay"
+
+STAGE = "Stage"
 # Permet de retrouver le bon chemin si on vient à changer le nom d'un folder
