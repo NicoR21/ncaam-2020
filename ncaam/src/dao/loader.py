f4d674f46814effabad4496c6908f43eb537a630
p.steffen <p.steffen@betclicgroup.com>
2020-04-10 15:27:17 +0200

    update loader.py

--- ncaam/src/dao/loader.py
+++ ncaam/src/dao/loader.py
@@ -1,12 +1,17 @@
 import pandas as pd
-from ncaam.src.constants import DATA_PATH
+from ncaam.src.constants import DATA_PATH, DATA_FILES, PLAY_BY_PLAY, STAGE
 
 
 class Loader:  # Nom de classe separe par une majuscule
     def __init__(self, local: bool = True) -> None:
         self.local = local
 
-    def import_data(self, table_name: str) -> tuple:
+    def import_data_files(self, table_name: str) -> (pd.DataFrame, pd.DataFrame):
         if self.local:
-            return pd.read_csv(f"{DATA_PATH}/MDataFiles_Stage1/{table_name}.csv"), \
-                   pd.read_csv(f"{DATA_PATH}/MDataFiles_Stage2/{table_name}.csv")
+            return pd.read_csv(f"{DATA_PATH}/{DATA_FILES}_{STAGE}1/{table_name}.csv"), \
+                   pd.read_csv(f"{DATA_PATH}/{DATA_FILES}_{STAGE}2/{table_name}.csv")
+
+    def import_play_by_play(self, table_name: str) -> (pd.DataFrame, pd.DataFrame):
+        if self.local:
+            return pd.read_csv(f"{DATA_PATH}/{PLAY_BY_PLAY}_{STAGE}1/{table_name}.csv"), \
+                   pd.read_csv(f"{DATA_PATH}/{PLAY_BY_PLAY}_{STAGE}2/{table_name}.csv")
