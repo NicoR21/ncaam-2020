afd94736974c9e6dbfe7d4e9852ea8ee8c631c7c
p.steffen <p.steffen@betclicgroup.com>
2020-04-10 18:09:28 +0200

    update constants.py

--- ncaam/src/constants.py
+++ ncaam/src/constants.py
@@ -1,7 +1,14 @@
 DATA_PATH = "ncaam/resources/data"
 
+# Data folders name
 DATA_FILES = "MDataFiles"
 PLAY_BY_PLAY = "MPlayByPlay"
+
+# Tables name
+EVENTS_TABLE = "MEvents"
+PLAYERS_TABLE = "MPlayers"
+CITIES_TABLE = "Cities"
+CONFERENCES_TABLE = "Conferences"
 
 STAGE = "Stage"
 # Permet de retrouver le bon chemin si on vient à changer le nom d'un folder
