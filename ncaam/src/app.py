10a82fa2cf6c3454af0793fa93fce2f07abecf7b
NicoR21 <nicolas.rousseau21000@gmail.com>
2020-04-22 11:48:15 +0200

    modif app.py

--- ncaam/src/app.py
+++ ncaam/src/app.py
@@ -12,7 +12,31 @@ from dash.dependencies import Input, Output
 
 #Répertoire de sauvegarde des fichiers bruts
 DATA_FILES_DIR = '../resources/data/MDataFiles_Stage2/'
-PLAY_BY_PLAY_DIR = '../resources/data/MPlayByPlay_Stage2'
+PLAY_BY_PLAY_DIR = '../resources/data/MPlayByPlay_Stage2/'
+
+#Tables
+CITIES_FILE = "Cities.csv"
+CONFERENCES_FILE = "Conferences.csv"
+MCONFERENCE_TOURNEY_GAMES_FILE = "ConferenceTourneyGamesTable.csv"
+MGAME_CITIES_FILE = "GameCities.csv"
+MMASSEY_ORDINALS_TABLE = "MMasseyOrdinals.csv"
+MNCAATOURNEY_COMPACT_RESULTS_FILE = "MNCAATourneyCompactResults.csv"
+MNCAATOURNEY_DETAILED_RESULTS_FIBLE = "MNCAATourneyDetailedResults.csv"
+MNCAATOURNEY_SEED_ROUND_SLOTS_FIBLE = "MNCAATourneySeedRoundSlots.csv"
+MNCAATOURNEY_SEEDS_FILE = "MNCAATourneySeeds.csv"
+MNCAATOURNEY_SLOTS_FILE = "MNCAATourneySlots.csv"
+MREGULAR_SEASON_COMPACT_RESULTS_FILE = "MRegularSeasonCompactResults.csv"
+MREGULAR_SEASON_DETAILED_RESULTS_FILE = "MRegularSeasonDetailedResults.csv"
+MSEASONS_FILE = "MSeasons.csv"
+MSECONDARY_TOURNEY_COMPACT_RESULTS_FILE = "MSecondaryTourneyCompactResults.csv"
+MSECONDARY_TOURNEY_TEAMS_FILE = "MSecondaryTourneyTeams.csv"
+MTEAM_COACHES_FILE = "MTeamCoaches.csv"
+MTEAM_CONFERENCES_FILE = "MTeamConferences.csv"
+MTEAMS_FILE = "MTeams.csv"
+MTEAM_SPELLING_FILE = "MTeamSpellings.csv"
+EVENTS_FILE = "MEvents.csv"
+PLAYERS_FILE = "MPlayers.csv"
+
 
 ENV_FILE='./env.yaml'
 with open(ENV_FILE) as f:
