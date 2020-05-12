dbacad802a7aed0699b00ee5ea7828355afd9a36
NicoR21 <nicolas.rousseau21000@gmail.com>
2020-05-12 16:12:57 +0200

    modif app

--- ncaam/src/app.py
+++ ncaam/src/app.py
@@ -1,8 +1,5 @@
 import os
 import numpy as np
-import yaml
-import datetime
-
 import pandas as pd
 
 import dash
@@ -14,37 +11,17 @@ from dash.dependencies import Input, Output
 DATA_FILES_DIR = '../resources/data/MDataFiles_Stage2/'
 PLAY_BY_PLAY_DIR = '../resources/data/MPlayByPlay_Stage2/'
 
-#Tables
-CITIES = "Cities.csv"
-CONFERENCES = "Conferences.csv"
-MCONFERENCE_TOURNEY_GAMES = "ConferenceTourneyGamesTable.csv"
-MGAME_CITIES = "GameCities.csv"
-MMASSEY_ORDINALS = "MMasseyOrdinals.csv"
-MNCAATOURNEY_COMPACT_RESULTS = "MNCAATourneyCompactResults.csv"
-MNCAATOURNEY_DETAILED_RESULTS = "MNCAATourneyDetailedResults.csv"
-MNCAATOURNEY_SEED_ROUND_SLOTS = "MNCAATourneySeedRoundSlots.csv"
-MNCAATOURNEY_SEEDS = "MNCAATourneySeeds.csv"
-MNCAATOURNEY_SLOTS = "MNCAATourneySlots.csv"
-MREGULAR_SEASON_COMPACT_RESULTS = "MRegularSeasonCompactResults.csv"
-MREGULAR_SEASON_DETAILED_RESULTS = "MRegularSeasonDetailedResults.csv"
-MSEASONS = "MSeasons.csv"
-MSECONDARY_TOURNEY_COMPACT_RESULTS = "MSecondaryTourneyCompactResults.csv"
-MSECONDARY_TOURNEY_TEAMS = "MSecondaryTourneyTeams.csv"
-MTEAM_COACHES = "MTeamCoaches.csv"
-MTEAM_CONFERENCES = "MTeamConferences.csv"
-MTEAMS = "MTeams.csv"
-MTEAM_SPELLING = "MTeamSpellings.csv"
-EVENTS = "MEvents.csv"
-PLAYERS = "MPlayers.csv"
+#Table principale
+df = 'df_2019.csv'
 
 
-#ENV_FILE='ncaam-2020/env.yaml'
-#with open(ENV_FILE) as f:
-#    params = yaml.load(f, Loader=yaml.FullLoader)
-
+# Lecture du fichier de données
+shot_df = pd.read_csv('df_2019.csv')
 
 # Colors of text and background
 colors = {'background': '#FFBB55', 'text': '#2255FF'}
+
+players=[{'label':p, 'value': p} for p in shot_df['FullName'].unique()]
 
 
 app = dash.Dash('NCAAM_2020')
@@ -55,16 +32,21 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
     ),
 
     html.Div(
-        children='Players Individual Statistics : 2015-2020',
+        children='Players Individual Statistics : 2019-2020',
         style={'textAlign': 'center', 'color': colors['text']}
-    )
+    ),
+
+    dcc.Tabs([
+        dcc.Tab(label='Players shots', children=[
+            html.Div([
+                dcc.Dropdown(
+                    id='player',
+                    options=players
+                )
+            ])
+        ])
+    ])
 ])
-
-
-
-
-
-
 
 
 
